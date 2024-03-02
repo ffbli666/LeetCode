425a9b7e304d32273c27875032ffef245550d77b
Zong-Ying Lyu <ffbli666@gmail.com>
2024-03-02 13:42:14 +0800

    add, 977. Squares of a Sorted Array

---
+++ 977. Squares of a Sorted Array/__test__/run2-two-pointer.spec.js
@@ -0,0 +1,9 @@
+const sortedSquares = require('../run2-two-pointerjs');
+describe('977. Squares of a Sorted Array', () => {
+    test('[-4,-1,0,3,10] to equal [0,1,9,16,100]', () => {
+        expect(sortedSquares([-4, -1, 0, 3, 10])).toEqual([0, 1, 9, 16, 100]);
+    });
+    test('[-7,-3,2,3,11] to equal [4,9,9,49,121]', () => {
+        expect(sortedSquares([-7, -3, 2, 3, 11])).toEqual([4, 9, 9, 49, 121]);
+    });
+});
