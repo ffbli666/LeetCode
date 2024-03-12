4a413ff7a186db8167e4c92cdd7253ad18331b19
Zong-Ying Lyu <ffbli666@gmail.com>
2024-03-12 12:06:39 +0800

    1. modify, 791 run3 調整成 map 查 index

--- 791. Custom Sort String/run3-custom-comparator.js
+++ 791. Custom Sort String/run3-custom-comparator.js
@@ -4,11 +4,16 @@
  * @return {string}
  */
 var customSortString = function (order, s) {
+    const map = {};
+    for (let i in order) {
+        map[order[i]] = i;
+    }
     return s.split('').sort((a, b) => {
-        let afind = order.indexOf(a) === -1 ? 99 : order.indexOf(a);
-        let bfind = order.indexOf(b) === -1 ? 99 : order.indexOf(b);
+        let afind = map[a] === undefined ? 99 : map[a];
+        let bfind = map[b] === undefined ? 99 : map[b];
         return afind - bfind;
     }).join('');
 };
 module.exports = customSortString;
 // 比1省記憶體,但比較慢
+// 調整 map 有快一點
